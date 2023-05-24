f9962ddff90e2a11bada688051430f1d3afeecf6
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-05-24 11:09:59 -0400

    add

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ function gamePlay(id){
     var td = document.getElementById(id)
     if (turn==0&& td.hasChildNodes()==false){
         var img = document.createElement('img')
-        img.setAttribute('src','pic2.jpg')
-        img.setAttribute('height','100px')
-        img.setAttribute('width','100px')
+        img.setAttribute('src','henry.jpg')//
+        img.setAttribute('height','120px')
+        img.setAttribute('width','120px')
         td.appendChild(img)
+     if (winner(img)==true){
+alert('Henry wins !')
+     }
+        
         if (draw()==true){
             alert ("The game is a draw")
         }
@@ -15,24 +19,26 @@ function gamePlay(id){
     }
     else if(turn == 1&& td.hasChildNodes()==false){
         var img = document.createElement('img')
-        img.setAttribute('src','pic1.png')
-        img.setAttribute('height','100px')
-        img.setAttribute('width','100px')
+        img.setAttribute('src','logan.jpg')
+        img.setAttribute('height','120px')
+        img.setAttribute('width','120px')
         td.appendChild(img)
+
+        if (winner(img)==true){
+            alert('Logan wins !')
+                 }
+
         if (draw()==true){
             alert ("The game is a draw")
         }
 
         turn = 0
 
-
-
-
     }
 }
 
 
-//draw function - we check every cell
+//draw function - we check every cell--noone win
 //first check every cell are blank or nor not or empty
 function draw(){
     for (i=0; i<=8; i++){
@@ -44,5 +50,95 @@ function draw(){
         }
     }
    return true
+
+}
+
+//function winner
+
+function winner(img){
+  
+    // first row
+    //check if all 3 boxes are filled in the first row position 0,1,2 
+    if (document.getElementById('0').hasChildNodes()&&document.getElementById('1').hasChildNodes()&& document.getElementById('2').hasChildNodes()){
+        //second if check if all 3 in the first row have the same images
+        // firstchild is inside td which is image tag
+if (document.getElementById('0').firstChild.src==img.src&& document.getElementById('1').firstChild.src==img.src&&document.getElementById('2').firstChild.src==img.src){
+    return true
+}
+    }
+//line 62 to 79 check row
+    // next second row 
+    if (document.getElementById('3').hasChildNodes()&&document.getElementById('4').hasChildNodes()&& document.getElementById('5').hasChildNodes()){
+        //second if check if all 3 in the first row have the same images
+        // firstchild is inside td which is image tag
+if (document.getElementById('3').firstChild.src==img.src&& document.getElementById('4').firstChild.src==img.src&&document.getElementById('5').firstChild.src==img.src){
+    return true
+}
+    }
+
+ // next thirs row 
+ if (document.getElementById('6').hasChildNodes()&&document.getElementById('7').hasChildNodes()&& document.getElementById('8').hasChildNodes()){
+    //second if check if all 3 in the first row have the same images
+    // firstchild is inside td which is image tag
+if (document.getElementById('6').firstChild.src==img.src&& document.getElementById('7').firstChild.src==img.src&&document.getElementById('8').firstChild.src==img.src){
+return true
+}
+}
+
+
+//check column 
+
+ // first column
+ if (document.getElementById('0').hasChildNodes()&&document.getElementById('3').hasChildNodes()&& document.getElementById('6').hasChildNodes()){
+    //second if check if all 3 in the first row have the same images
+    // firstchild is inside td which is image tag
+if (document.getElementById('0').firstChild.src==img.src&& document.getElementById('3').firstChild.src==img.src&&document.getElementById('6').firstChild.src==img.src){
+return true
+}
+}
+
+
+// second column
+if (document.getElementById('1').hasChildNodes()&&document.getElementById('4').hasChildNodes()&& document.getElementById('7').hasChildNodes()){
+    //second if check if all 3 in the first row have the same images
+    // firstchild is inside td which is image tag
+if (document.getElementById('1').firstChild.src==img.src&& document.getElementById('4').firstChild.src==img.src&&document.getElementById('7').firstChild.src==img.src){
+return true
+}
+}
+
+// second column
+if (document.getElementById('2').hasChildNodes()&&document.getElementById('5').hasChildNodes()&& document.getElementById('8').hasChildNodes()){
+    //second if check if all 3 in the first row have the same images
+    // firstchild is inside td which is image tag
+if (document.getElementById('2').firstChild.src==img.src&& document.getElementById('5').firstChild.src==img.src&&document.getElementById('8').firstChild.src==img.src){
+return true
+}
+}
+
+
+// for cross
+if (document.getElementById('0').hasChildNodes()&&document.getElementById('4').hasChildNodes()&& document.getElementById('8').hasChildNodes()){
+    //second if check if all 3 in the first row have the same images
+    // firstchild is inside td which is image tag
+if (document.getElementById('0').firstChild.src==img.src&& document.getElementById('4').firstChild.src==img.src&&document.getElementById('8').firstChild.src==img.src){
+return true
+}
+}
+
+// another cross
+if (document.getElementById('2').hasChildNodes()&&document.getElementById('4').hasChildNodes()&& document.getElementById('6').hasChildNodes()){
+    //second if check if all 3 in the first row have the same images
+    // firstchild is inside td which is image tag
+if (document.getElementById('2').firstChild.src==img.src&& document.getElementById('4').firstChild.src==img.src&&document.getElementById('6').firstChild.src==img.src){
+return true
+}
+}
+
+
+return false
+
+
+
 
 }
